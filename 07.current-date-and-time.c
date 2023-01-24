c9b86723b550dba5292c333ea11d958f2210f142
someoneelsebg <someoneelsebg@outlook.com>
2023-01-24 17:26:10 +0200

    the problem is solved

--- 07.current-date-and-time.c
+++ 07.current-date-and-time.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include <time.h>
 
 int main() {
+    time_t rawtime;
+    struct tm *timeinfo;
+    char buffer[80];
+
+    time(&rawtime);
+    timeinfo = localtime(&rawtime);
+
+    strftime(buffer, 80, "%d %B %Y %H:%M:%S", timeinfo);
+    printf("%s\n", buffer);
 
     return 0;
 }
